c8a698d6b1fb18a376e0485d9abc80ac20441109
Erregard <38902628+Erregard@users.noreply.github.com>
2019-09-17 17:31:31 +0700

    Add files via upload

---
+++ pertmuan1.php
@@ -0,0 +1,42 @@
+<?php
+//Penggabungan String / Concat
+// $nama_depan = "Chandra ";
+// $nama_belakang= "Syahputra";
+
+// echo $nama_depan . " ". $nama_belakang;
+
+// ===OPERATOR ASSIGNMENT===
+// =, +=
+
+// $x = 1;
+// $x += 5; //(x =1, lalu x nya ditambah 5, 1+5)
+// echo $x;
+
+// $x = "Nama ";
+// $x .= "Saya ";
+// $x .= "Chandra";
+
+// echo $x;
+
+?>
+
+<?php
+
+//OPERATOR PERBANDINGAN
+// <, >, <=, =>, ==
+// == -> Ngecek nilai
+
+// var_dump(1 == "1"); //mengecek nilai saja
+
+// var_dump(1 === "1"); //mengecek tipe datanya juga
+?>
+
+<?php
+//LOGIKA
+// &&, ||, !
+$x =30;
+
+// var_dump($x > 20 || $x /10 == 0);
+var_dump($x < 20 && $x /10 == 3);
+
+?>
